ba4ca2f43254c724d2e41af2d523b3ef35fd943f
braveseokyung <sk100@korea.ac.kr>
2024-10-04 21:18:17 +0900

    [style] 문단에 icon 추가

--- frontend/real-estate/src/routes/apartment.js
+++ frontend/real-estate/src/routes/apartment.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import DetailInfo from "../components/DetailInfo";
 import { useParams } from "react-router-dom";
-import { car, house } from "../icon";
+import { env, community, attribute, traffic,infra, school, sound, parking } from "../icon";
 
 const Apartment = () => {
-  const icons = [car, house];
+  // const icons = [env, community, attribute, infra, traffic, school, sound, parking];
   const info = `Lorem Ipsum은 인쇄 및 조판 산업에서 단순한 더미 텍스트입니다. Lorem Ipsum은 1500년대에 한 무명의 인쇄공이 활자 배열을 뒤섞어 본보기 서적을 만들면서부터 업계 표준 더미 텍스트로 자리 잡았습니다. 이 텍스트는 5세기 동안 변함없이 살아남았을 뿐만 아니라 전자 조판으로의 도약도 이루었습니다. 1960년대에는 Lorem Ipsum 구절이 담긴 Letraset 시트가 출시되면서 널리 알려졌으며, 최근에는 Aldus PageMaker와 같은 데스크톱 출판 소프트웨어에도 Lorem Ipsum의 다양한 버전이 포함되었습니다.`;
   const { apartmentId } = useParams();
   const [apt, setApt] = useState({});
@@ -36,7 +36,7 @@ const Apartment = () => {
   return (
     <div className="bg-slate-100">
       <div class="flex">
-        <Sidebar icons={icons} />
+        <Sidebar />
         <div className="mt-3 p-[2%] mx-[2vw] ml-[18vw]">
           <div className="flex rounded-2xl p-3">
             <div>
@@ -69,19 +69,19 @@ const Apartment = () => {
             <div>No data available</div>
           )}
         </div>
-        <DetailInfo id="A-env" title="환경(단지,조경)" info={info} icon={car} />
+        <DetailInfo id="A-env" title="환경(단지,조경)" info={info} icon={env} />
         <DetailInfo
           id="A-community"
           title="커뮤니티"
           info={info}
-          icon={house}
+          icon={community}
         />
-        <DetailInfo id="A-attribute" title="동별 특징" info={info} icon={car} />
-        <DetailInfo id="A-infra" title="주변 상권" info={info} icon={house} />
-        <DetailInfo id="A-traffic" title="교통" info={info} icon={car} />
-        <DetailInfo id="A-school" title="학군" info={info} icon={house} />
-        <DetailInfo id="A-sound" title="소음" info={info} icon={car} />
-        <DetailInfo id="A-parking" title="주차" info={info} icon={house} />
+        <DetailInfo id="A-attribute" title="동별 특징" info={info} icon={attribute} />
+        <DetailInfo id="A-infra" title="주변 상권" info={info} icon={infra} />
+        <DetailInfo id="A-traffic" title="교통" info={info} icon={traffic} />
+        <DetailInfo id="A-school" title="학군" info={info} icon={school} />
+        <DetailInfo id="A-sound" title="소음" info={info} icon={sound} />
+        <DetailInfo id="A-parking" title="주차" info={info} icon={parking} />
       </div>
     </div>
   );
